37cad1e31d81091a36b60602efbb4a106c5ab9bd
Elizabeth Klenczar <eklenczar@gmail.com>
2022-03-21 14:40:56 -0400

    commit tree e142ab4c0feba75394ac7b007d82d1cb9a81a129

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+
+
+const renderFocus = () => {
+    console.log("Good!")
+}
+
+const renderBlur = () => {
+    console.log("Hey! Eyes on me!")
+}
+
+const EyesOnMe = () => {
+  return (
+    <div>
+        <button onFocus={renderFocus} onBlur={renderBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
